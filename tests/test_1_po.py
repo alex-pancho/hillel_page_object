4183fd13f138d33c282524214b4d22ab99fd8fc3
Tesliuk Vlad <vvv3410@gmail.com>
2024-08-31 08:23:28 +0300

    last HW

--- tests/test_1_po.py
+++ tests/test_1_po.py
@@ -16,3 +16,16 @@ def test_fail_path(home_page):
     with pytest.raises(AttributeError):
         element = home_page.item("sign_in_button2")
         element.is_visible()
+
+def test_contact_header_visible(home_page):
+    element = home_page.item("contacts_head")
+    assert element.is_visible(), f"Not found: {element._locator}"
+
+def test_contact_section_visible(home_page):
+    element = home_page.item("contact_section")
+    assert element.is_visible(), f"Not found: {element._locator}"
+
+def test_homepage_sign_up(home_page):
+    element = home_page.item("sign_up_button")
+    assert element.is_visible(), f"Not found: {element._locator}"
+
